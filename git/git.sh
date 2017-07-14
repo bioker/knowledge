74f07488ee9f19128499489c37c8369aaeedabda
Viktor Vlasov <viktorvlasovsiberian@gmail.com>
2017-07-14 23:39:55 +0300

    git log format alias

--- git/git.sh
+++ git/git.sh
@@ -2,7 +2,7 @@
 git config --global user.name "Viktor Vlasov"
 git config --global user.email "viktorvlasovsiberian@gmail.com"
 git config --global alias.hist "log --pretty=oneline --graph --decorate --all"
-git config --global alias.datehist "log --pretty='format:%C(auto)%h %Cred%cd%Creset %Cblue%an%Creset %s %d' --graph --all --date=format:%F_%T"
+git config --global alias.l "log --pretty='format:%C(auto)%h %G? %Cgreen%cd%Creset %Cblue%an%Creset %s %C(auto)%d' --graph --all --date=format:%F_%T"
 git config --global core.autocrlf input
 git config --global core.quotepath false
 git config --global push.default simple
