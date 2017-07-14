a4dc32fd4fe7982860c842f8df838dda77d0811f
Viktor Vlasov <viktorvlasovsiberian@gmail.com>
2017-07-14 23:09:34 +0300

    git signing key config

--- git/git.sh
+++ git/git.sh
@@ -10,3 +10,4 @@ git config --global diff.tool vimdiff
 git config --global merge.tool vimdiff
 git config --global merge.conflictstyle diff3
 git config --global mergetool.prompt false
+git config --global user.signingkey C1A1019C
