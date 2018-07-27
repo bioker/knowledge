6cc9ef06e4281ca8d6131dc82a3e230ad317e7da
Viktor Vlasov <viktorvlasovsiberian@gmail.com>
2018-07-27 17:42:29 +0300

    gnome-terminal appearance scripts

---
+++ bash/gnome-terminal/gnome-terminal-dark.sh
@@ -0,0 +1,1 @@
+cat gnome-terminal-dark.dconf | dconf load /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/
---
+++ bash/gnome-terminal/gnome-terminal-light.sh
@@ -0,0 +1,1 @@
+cat gnome-terminal-light.dconf | dconf load /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/
